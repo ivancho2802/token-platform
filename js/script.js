834a74e1a9860eb95fa6896b3175732829dcfaa3
Ivan Diaz <ivancho2802@hotmail.com>
2020-10-19 10:49:00 -0500

    guardar cambios de la plataforma segunra revision remota

--- js/script.js
+++ js/script.js
@@ -71,9 +71,47 @@ function getTableUsers (followersData, cities){
         <td class="text-center">${cityaux}</td>
         <td class="text-center">${genereaux}</td>
         <td class="text-center">${edad}</td>
-        <td class="text-center">
-            <button type="button" id="PopoverCustomT-2" class="btn btn-primary btn-sm">Details</button>
+      </tr>
+      `;
+      container += eltr
+    }
+  }
+  return container;
+}
+function getTableUsersAct (usersData, cities){
+
+  var userArray = usersData ? usersData.fk_user_asocd : []
+
+  var container="" , eltr , edad, currentdateyear = new Date().getFullYear(), genere = {male : "Hombre", female: "Mujer"}, genereaux, cityaux;
+  if(!userArray.length || !cities.length)
+    return '<div class="d-block text-center card-footer">No se encontraron Usuarios</div>';
+  for (var i = 0; i < userArray.length; i++) {
+    if(userArray[i]){
+      edad = new Date(userArray[i].fecha_nac).getFullYear() ? currentdateyear - new Date(userArray[i].fecha_nac).getFullYear() : "No Data";
+      genereaux = genere[userArray[i].genero] ? genere[userArray[i].genero] : "No Data";
+      cityaux = userArray[i].address && cities.findIndex(city => city._id === userArray[i].address) !== -1 ? cities[cities.findIndex(city => city._id === userArray[i].address)].city +" - "+ cities[cities.findIndex(city => city._id === userArray[i].address)].country : "No Data";
+      eltr = '';
+      eltr = `
+      <tr>
+        <td class="text-center text-muted">${i}</td>
+        <td>
+          <div class="widget-content-wrapper">
+            <div class="widget-content p-0">
+              <div class="widget-content-left mr-3">
+                  <div class="widget-content-left">
+                      <img width="40" class="rounded-circle" src="${userArray[i].image}" alt="">
+                  </div>
+              </div>
+              <div class="widget-content-left flex2">
+                  <div class="widget-heading">${userArray[i].name} ${userArray[i].lastname}</div>
+                  <div class="widget-subheading opacity-7">${userArray[i].email}</div>
+              </div>
+            </div>
+          </div>
         </td>
+        <td class="text-center">${cityaux}</td>
+        <td class="text-center">${genereaux}</td>
+        <td class="text-center">${edad}</td>
       </tr>
       `;
       container += eltr
@@ -137,12 +175,223 @@ function groupByWeek(statics){
   return formatgroups.reverse()
 }
 
+function groupByWeekStatics(acumCuponClientActivos, acumCuponClientActivosVencido, acumCuponClientScan){
+
+  var auxScan,  auxVenc,  auxAct , 
+  dates = [], 
+  semanaMayor,  semanaMenor, 
+  labelsStatics =[], 
+  dataCuponsClienteScan=[],   dataCuponsClienteVencido=[],   dataCuponsClienteActivo=[],
+  cuponsClienteActivo=[],   cuponsClienteScan=[],   cuponsClienteVencido=[];
+
+  if(acumCuponClientActivos.length) 
+     cuponsClienteActivo = groupByWeek(acumCuponClientActivos)
+  if(acumCuponClientActivosVencido.length) 
+     cuponsClienteVencido = groupByWeek(acumCuponClientActivosVencido)
+  if(acumCuponClientScan.length) 
+     cuponsClienteScan = groupByWeek(acumCuponClientScan)
+
+  auxScan = cuponsClienteScan.length ? cuponsClienteScan[cuponsClienteScan.length-1].week:'',
+  auxVenc = cuponsClienteVencido.length ? cuponsClienteVencido[cuponsClienteVencido.length-1].week : '',
+  auxAct = cuponsClienteActivo.length ? cuponsClienteActivo[cuponsClienteActivo.length-1].week:''
+
+
+  if(auxScan)dates.push( auxScan.split('-').pop()); 
+  if(auxAct)dates.push( auxAct.split('-').pop()); 
+  if(auxVenc)dates.push( auxVenc.split('-').pop());  
+
+  semanaMayor = Math.max.apply(null, dates);
+  semanaMenor = Math.min.apply(null, dates);
+  // var formatsemanaMayor = '2020'
+  var j = 0
+  for (var i = semanaMenor; i < semanaMayor+1; i++) {
+
+      labelsStatics.push("Semana "+i);
+
+      if(cuponsClienteActivo.length)
+      cuponsClienteActivo.forEach(element => {
+          if(element.week.split('-').pop() == i)
+              dataCuponsClienteActivo.push(element.data.length)
+      });
+      if(!dataCuponsClienteActivo[j])
+          dataCuponsClienteActivo.push(0)
+
+      if(cuponsClienteScan.length)
+      cuponsClienteScan.forEach(element => {
+          if(element.week.split('-').pop() == i)
+              dataCuponsClienteScan.push(element.data.length)
+      });
+      if(!dataCuponsClienteScan[j])
+          dataCuponsClienteScan.push(0)
+
+      if(cuponsClienteVencido.length)
+      cuponsClienteVencido.forEach(element => {
+          if(element.week.split('-').pop() == i)
+              dataCuponsClienteVencido.push(element.data.length)
+      });
+      if(!dataCuponsClienteVencido[j])
+          dataCuponsClienteVencido.push(0)
+      j++;
+  }
+
+  bodyStatics = {
+      labels: labelsStatics,//["Semana: 40", "Semana: 41", "Semana: 42"],//
+      datasets: [{//staticsHomeData
+          label: "Redimidos",
+          backgroundColor: "green",//window.chartColors.red,
+          data: dataCuponsClienteScan//[6, 0, 0]//
+      }, {
+          label: "Activos",
+          backgroundColor: "blue",//window.chartColors.blue,
+          data: dataCuponsClienteActivo//[4, 5, 3] // 
+      } , {
+          label: "Vencidos",
+          backgroundColor: "red",//window.chartColors.blue,
+          data: dataCuponsClienteVencido//[4, 5, 3]//
+      } 
+      ]
+  }
+
+  return bodyStatics
+}
+
+function groupByAdress(statics, cities){
+  const groups = statics.reduce((acc, data) => {
+
+    var address = data.fk_cliente.address && cities.findIndex(city => city._id === data.fk_cliente.address) !== -1 ? cities[cities.findIndex(city => city._id === data.fk_cliente.address)].city +" - "+ cities[cities.findIndex(city => city._id === data.fk_cliente.address)].country : "No Dir";
+    // const address = data.fk_cliente.address
+
+    if (!acc[address]) {
+      acc[address] = [];
+    }
+    
+    acc[address].push(data);
+
+    return acc;
+
+  }, {});
+
+  var formatgroups = [], key, value
+
+  for (var i = 0; i < Object.keys(groups).length; i++) {
+    key = Object.keys(groups)[i];
+    value = Object.values(groups)[i];
+    formatgroups.push({address: key, data: value})
+  }
+  // console.log(formatgroups);
+  return formatgroups.reverse()
+}
+
+function groupAll(statics){
+  const groups = statics.reduce((acc, data) => {
+
+    const address = data.address
+
+    if (!acc[address]) {
+      acc[address] = [];
+    }
+    
+    acc[address].push(data);
+
+    return acc;
+
+  }, {});
+
+  var formatgroups = [], key, value
+
+  for (var i = 0; i < Object.keys(groups).length; i++) {
+    key = Object.keys(groups)[i];
+    value = Object.values(groups)[i];
+    formatgroups.push({address: key, data: value})
+  }
+  // console.log(formatgroups);
+  return formatgroups.reverse()
+}
+function groupByAdressStatics(acumCuponClientActivos, acumCuponClientActivosVencido, acumCuponClientScan, citiesData){
+
+  var auxScan,  auxVenc,  auxAct , 
+  dates = [], 
+  semanaMayor,  semanaMenor, 
+  labelsStatics =[], 
+  dataCuponsClienteScan=[],   dataCuponsClienteVencido=[],   dataCuponsClienteActivo=[],
+  cuponsClienteActivo=[],   cuponsClienteScan=[],   cuponsClienteVencido=[];
+  
+  if(acumCuponClientActivos.length) 
+     cuponsClienteActivo = groupByAdress(acumCuponClientActivos, citiesData)
+  if(acumCuponClientActivosVencido.length) 
+     cuponsClienteVencido = groupByAdress(acumCuponClientActivosVencido, citiesData)
+  if(acumCuponClientScan.length) 
+     cuponsClienteScan = groupByAdress(acumCuponClientScan, citiesData)
+
+  // auxScan = cuponsClienteScan.length ? cuponsClienteScan[cuponsClienteScan.length-1].adress:'',
+  // auxVenc = cuponsClienteVencido.length ? cuponsClienteVencido[cuponsClienteVencido.length-1].adress : '',
+  // auxAct = cuponsClienteActivo.length ? cuponsClienteActivo[cuponsClienteActivo.length-1].adress:''
+
+  // if(auxScan)dates.push( auxScan.split('-').pop()); 
+  // if(auxAct)dates.push( auxAct.split('-').pop()); 
+  // if(auxVenc)dates.push( auxVenc.split('-').pop());  
+
+  cuponesMayor = groupAll(cuponsClienteScan.concat(cuponsClienteVencido).concat(cuponsClienteActivo));
+  // semanaMenor = Math.min.apply(null, dates);
+  // var formatsemanaMayor = '2020'
+  var j = 0
+  for (var i = 0; i < cuponesMayor.length; i++) {
+
+      labelsStatics.push(cuponesMayor[i].address);
+
+      if(cuponsClienteActivo.length)
+      cuponsClienteActivo.forEach(element => {
+          if(element.address == cuponesMayor[i].address)
+            dataCuponsClienteActivo.push(element.data.length)
+      });
+      if(!dataCuponsClienteActivo[j])
+          dataCuponsClienteActivo.push(0)
+
+      if(cuponsClienteScan.length)
+      cuponsClienteScan.forEach(element => {
+          if(element.address == cuponesMayor[i].address)
+            dataCuponsClienteScan.push(element.data.length)
+      });
+      if(!dataCuponsClienteScan[j])
+          dataCuponsClienteScan.push(0)
+
+      if(cuponsClienteVencido.length)
+      cuponsClienteVencido.forEach(element => {
+          if(element.address == cuponesMayor[i].address)
+              dataCuponsClienteVencido.push(element.data.length)
+      });
+      if(!dataCuponsClienteVencido[j])
+          dataCuponsClienteVencido.push(0)
+      j++;
+  }
+
+  bodyStatics = {
+      labels: labelsStatics,//["Semana: 40", "Semana: 41", "Semana: 42"],//
+      datasets: [{//staticsHomeData
+          label: "Redimidos",
+          backgroundColor: "green",//window.chartColors.red,
+          data: dataCuponsClienteScan//[6, 0, 0]//
+      }, {
+          label: "Activos",
+          backgroundColor: "blue",//window.chartColors.blue,
+          data: dataCuponsClienteActivo//[4, 5, 3] // 
+      } , {
+          label: "Vencidos",
+          backgroundColor: "red",//window.chartColors.blue,
+          data: dataCuponsClienteVencido//[4, 5, 3]//
+      } 
+      ]
+  }
+
+  return bodyStatics
+}
+
 var DECIMAL_SEPARATOR=".";
 var GROUP_SEPARATOR=",";
   
 function  unFormat(val) {
-  console.log("unFormat")
-  console.log(val)
+  // console.log("unFormat")
+  // console.log(val)
     if (!val) {
         return;
     }
@@ -164,4 +413,24 @@ function format(valString) {
   return parts[0].replace(/\B(?=(?:\d{3})+(?!\d))/g, this.GROUP_SEPARATOR) + (!parts[1] ? '' : this.DECIMAL_SEPARATOR + parts[1]);
 };
 
+function acumElementOfArrayObject(array, objectacum, formatResult){
+  var suma = 0
+  array.forEach (function(element){
+      suma += element[objectacum];
+  });
+  if(formatResult)
+    return format(suma);
+  else
+    return suma;
+}
+function acumElementIIILOfArrayObject(array, objectacumlevel1, objectacumlevel2,objectacumlevel3, formatResult){
+  var suma = 0
+  array.forEach (function(element){
+      suma += element[objectacumlevel1][objectacumlevel2][objectacumlevel3];
+  });
+  if(formatResult)
+    return format(suma);
+  else
+    return suma;
+}
   
