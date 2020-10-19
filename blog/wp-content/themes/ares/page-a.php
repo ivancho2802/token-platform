36fbdcc8988121e95a8aed6a2d4677f4f95b36dd
Ivan Diaz <ivancho2802@hotmail.com>
2020-10-19 17:24:48 -0500

    cambios shellby con wordpress

---
+++ blog/wp-content/themes/ares/page-a.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * Template name: Template A
+ */
+
+get_header(); ?>
+
+    <div id="primary" class="content-area">
+
+        <main id="main" class="site-main">
+
+            <div class="container">
+
+                <div class="frontpage">
+
+                    <?php while ( have_posts() ) : the_post(); ?>
+
+                        <?php
+
+                        $ares_options = ares_get_options();
+                        $alternate_layout = isset( $ares_options['single_post_layout_style'] ) && $ares_options['single_post_layout_style'] == 'alternate' ? true : false;
+
+                        ?>
+
+                        <div class="homepage-content">
+
+                            <div class="row">
+
+                                <div class="col-md-<?php echo $ares_options['ares_single_layout'] == 'col2r' && is_active_sidebar(1) ? '8' : '12'; ?>">
+
+                                    <?php if ( has_post_thumbnail() && $ares_options['ares_single_featured'] == 'on' && $alternate_layout ) : ?>
+
+                                        <div class="featured-image">
+
+                                            <img class="feat-img" src="<?php echo esc_url( get_the_post_thumbnail_url( get_the_ID(), 'large' ) ); ?>" alt="<?php esc_html( get_the_title() ); ?>">
+
+                                        </div>
+
+                                    <?php endif; ?>
+
+                                    <article id="post-<?php the_ID(); ?>">
+
+                                        <header class="entry-header">
+                                            <?php the_title( '<h2 class="post-title ' . ( ( $alternate_layout ) ? 'alt-layout' : '' ) . '">', '</h2>' ); ?>
+                                            <div class="avenue-underline"></div>
+                                        </header><!-- .entry-header -->
+
+                                        <div class="entry-content">
+                                            <?php the_content(); ?>
+                                            <?php
+
+                                            if( is_active_sidebar('sidebar-a')) :
+
+                                                dynamic_sidebar('sidebar-a');
+
+                                            endif;
+
+                                            wp_link_pages( array(
+                                                'before' => '<div class="page-links">' . __( 'Pages:', 'ares' ),
+                                                'after' => '</div>',
+                                            ) );
+                                            ?>
+                                        </div><!-- .entry-content -->
+                                        <footer class="entry-footer">
+                                            <?php edit_post_link( __( 'Edit', 'ares' ), '<span class="edit-link">', '</span>' ); ?>
+                                        </footer><!-- .entry-footer -->
+
+                                    </article><!-- #post-## -->
+
+                                </div>
+
+                                <?php if ( $ares_options['ares_single_layout'] == 'col2r' && is_active_sidebar(1) ) : ?>
+
+                                    <div class="col-md-4 avenue-sidebar">
+                                        <?php get_sidebar(); ?>
+                                    </div>
+
+                                <?php endif; ?>
+
+                            </div>
+
+                        </div>
+
+
+                        <?php
+
+                        // If comments are open or we have at least one comment, load up the comment template
+                        if (comments_open() || '0' != get_comments_number()) :
+                            comments_template();
+                        endif;
+
+                        ?>
+
+                    <?php endwhile; // end of the loop.   ?>
+
+
+
+                </div>
+
+            </div>
+
+        </main><!-- #main -->
+
+    </div><!-- #primary -->
+
+<?php
+get_footer();
---
+++ contacto.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/style.css">
+    <title>Token</title>
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
+    
+</head>
+<body>
+    <div class="theme-switch-wrapper">
+        <label class="theme-switch" for="checkbox">
+            <input type="checkbox" id="checkbox" />
+            <div class="slider round"></div>
+        </label>
+    </div>
+    <section id="main">
+        <div id="navegacion">
+            <nav>
+                <div>
+                    <img src="./sources/webToken-11.png" alt="">
+                </div>
+                <div id="enlaces">
+                    <div><a href="index.php">Inicio</a></div>
+                    <div><a href="usuarios.html">Usuarios</a></div>
+                    <div><a href="negocios.html">Negocios</a></div>
+                    <div><a href="http://localhost/token-platform-shellby/blog">Blog</a></div>
+                    <div><a href="contacto.php">Contacto</a></div>
+                    <div><a href="iniciarsesion.php">Inicia Sesión</a></div>
+                </div>
+            </nav>
+        </div>
+        <div id="usuarios_init">
+            <div>
+            <?php
+                if(isset($_POST['submit'])){
+                  // Aqui coloca los campos que quieres enviar por correo
+                  $email = $_POST['email'];    // 'email' es el name del input
+                  $nombre =  $_POST['nombre'];
+                  $apellido =  $_POST['apellido'];
+                  $mensaje =  $_POST['mensaje'];
+
+                  if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+
+                        $asunto = ''.$nombre.'quiere ponerse en contacto';
+                        $contenido = '
+                                <html>
+                                <head>
+                                <title></title>
+                                </head>
+                                <body>
+                                <hr>
+                                <h1> Citas - Clínica Sandiego Cúcuta</h1>
+                                <h6> www.clinicasandiegocucuta.com </h6>
+                                <hr>
+                                <p>'.$nombre.', esta interesado la aplicacion. <br>
+                                <br>
+                                Información de contacto:
+                                <br>
+                                    Identificación: '.$apellido.' <br>
+                                    Teléfono:  '.$email.' <br>
+                                    E-mail: '.$mensaje.' <br>
+                                </p>
+                                <hr>
+                                </body>
+                                </html>
+                            ';
+                        $headers = 'MIME-Version: 1.0' . "\r\n";
+                        $headers .= "Content-type: text/html; charset=UTF-8\r\n";
+
+                        $send = mail('desarrollobarter@gmail.com',$asunto,$contenido,$headers);
+                        if($send){
+                            $miresultado = '<h4 class="ok_send">¡Tu solicitud ha sido enviada!</h4>';
+                        }else{
+                            $miresultado = '<h5>No se envío tu solicitud, intenta más tarde.</h5>';
+                        }
+                        }else{
+                        $miresultado = '<h5>Oops, el email no es valido.</h5>';
+                        }
+                    }
+
+
+                ?>
+                <h1>Conctacto</h1>
+                <form action="<?php $_SERVER['PHP_SELF']?>" method="POST" >
+                    <div>
+                        <div>
+                            <label for="">Nombre</label>
+                            <input name="nombre"  class="form-contact" type="text">
+                        </div>
+                        <div>
+                            <label name="apellido" for="">Apellidos</label>
+                            <input name="nombre"  class="form-contact" type="text">
+                        </div>
+                    </div>
+                    <div>
+                        <label  name="email" for="">Email</label>
+                        <input name="nombre"  class="form-contact" type="email">
+                    </div>
+                    <div>
+                        <label name="" for="">Escribe tu mensaje</label>
+                        <textarea  name="mensaje"  class="form-contact-text" rows="4" cols="10" type="text"> </textarea>
+                    </div>  
+                    <button type="submit" class="btn-contact">Enviar</button>
+                </form>
+            </div>
+            <div>
+                <img id="usr_phone" src="./sources/ce2.png" alt="">
+            </div>
+        </div>
+    </section>
+    
+ 
+  
+    
+   <section id="usr_descarga" >
+ 
+    <div>
+        <h1>Descubre los TOKENS
+            disponibles en las principales
+            plataformas</h1>
+        <div>
+            <img src="./sources/webToken-16.svg" alt="">
+            <img src="./sources/webToken-18.svg" alt="">
+        </div>
+    </div>
+    <img src="./sources/cel2.png" alt="">
+</section>
+
+    <section id="footer">
+        <div>
+            <img src="./sources/webToken-11.png" alt="">
+            <p>¡Disfruta de todos los servicios que token tiene para ti!</p>
+        </div>
+        <div>
+            <form action="">
+                <label for="">Recibe las ultimas 
+                    actualizaciones</label><br>
+                <input type="text" placeholder="Aquí tu correo">
+                <button><img src="./sources/webToken-13.svg" alt=""></button>
+            </form>
+        </div>
+    </section>
+    <section id="copy"> <p>Copyright ©2020 Todos los derechos reservados</p> </section>
+   
+    <script src="./js/script.js"></script>
+    <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+    <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js'></script>
+   
+
+<script>
+    const colorSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+    function switchColor(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }
+    }
+    colorSwitch.addEventListener('change', switchColor, false);
+
+</script>
+
+</body>
+</html> 
